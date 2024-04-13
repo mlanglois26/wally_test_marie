6288e9ec92bb5b18245ffd35fc3aa3b964e81e2a
marie langlois <marie.langlois2@gmail.com>
2024-04-13 10:52:19 +0200

    sign up ok

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  enum role: { admin: 0, tenant: 1, owner: 2}
+end
